1dc483b402cc909a42f2cf88f08fd9a0bf6cfa70
Mariangel <marimantilla1@gmail.com>
2017-03-19 00:17:24 -0430

    se acaba de realizar un bucle for para el registro de los datos de las vacas

--- 1er proyecto.cpp
+++ 1er proyecto.cpp
@@ -65,5 +65,32 @@ float getlitros(){
     }
 };
 int main(int argc, char** argv) {
+	int nVacas,opc;
+	Vacas LVacas[Limite]; 
+	Vacas auxiliar;
+	cout<<"Cuantas Vacas Desea Ingresar?\n->:";
+	cin>>nVacas;
+	system("cls");
+	
+	for(int x=0;x<nVacas;x++){
+		string nom,raz;
+    	float pro,pes;   
+       
+   		cout<<"\nVaca Numero: "<<x+1<<endl;
+   		cout<<"\n nombre : ";
+   		cin>>nom;
+   		LVacas[x].setnombre(nom);
+   		cout<<"\n raza : ";
+   		cin>>raz;
+   		LVacas[x].setraza(raz);
+   		cout<<"\n peso : ";
+   		cin>>pes;
+   		LVacas[x].setpeso(pes);
+   		cout<<"\n promedio de leche : ";
+   		cin>>pro;
+   		LVacas[x].setlitros(pro);
+   		system("cls");
+	}
+	//HASTA AQUI MARIANGEL	
 	return 0;
 }
