dd23892f86ff454f468c451681792cad4dd5f7ae
Mariangel <marimantilla1@gmail.com>
2017-03-19 00:08:53 -0430

    librerias, definiciones y algunas variables con las que se trabajara en el proyecto

--- 1er proyecto.cpp
+++ 1er proyecto.cpp
@@ -1,6 +1,19 @@
+#include <cstdlib>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#ifndef Limite
+#define Limite 1000
+#endif
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+
+
+using namespace std;
+int n=0,ncuantos,resultado;
 
 int main(int argc, char** argv) {
 	return 0;
