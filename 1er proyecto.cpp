637fcd21ded56f3c24c70ec9058b97be8d9cdfb5
Yilmar <yilmar22villamizar@gmail.com>
2017-03-19 00:13:24 -0430

    Aqui se observa la implementacion de la clase

--- 1er proyecto.cpp
+++ 1er proyecto.cpp
@@ -15,6 +15,55 @@ using namespace std;
 using namespace std;
 int n=0,ncuantos,resultado;
 
+class Vacas {
+public:
+
+//constructor y destructor
+Vacas() {}
+~Vacas(){}
+
+private:
+
+string v_nombre;
+string v_raza;
+float v_peso;
+float  v_litros;
+
+
+public:
+       
+
+void setnombre(string nombre){
+	 v_nombre=nombre;
+ }
+string getnombre(){
+    return v_nombre;
+}
+    
+void setraza( string raza){
+	v_raza = raza;
+}
+string getraza(){
+    return v_raza;
+}
+    
+void setpeso(float p) {
+	v_peso=p;
+}
+
+float getpeso(){
+    return v_peso;
+    }   
+    
+
+void setlitros( float l) 
+{ v_litros= l;
+}
+float getlitros(){
+    return v_litros;
+    
+    }
+};
 int main(int argc, char** argv) {
 	return 0;
 }
